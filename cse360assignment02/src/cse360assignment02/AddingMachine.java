b31d6cbdee26d8ddaf0d2f12465ea87af6fe5ec9
RaghavAggarwal1 <43023682+RaghavAggarwal1@users.noreply.github.com>
2020-10-05 22:00:12 -0700

    Initial Check

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,41 @@
+/**
+* The AddingMaschine program is for CSE 360 class
+* for practice with using a version control system.
+*
+* @author  Raghav Aggarwal, CSE 360 faculty and ASU faculty
+* @version 1.0
+* @since   2020-10-05
+*/
+package cse360assignment02;
+
+//Making a class AddingMachine
+public class AddingMachine {
+	private int total;
+	  
+	//function to add total
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  //Function to get value of total
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  //Function to add
+	  public void add (int value) {
+	  }
+
+	  //function to add
+	  public void subtract (int value) {
+	  }
+
+	  //function to get string
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  //function to clear screen
+	  public void clear() {
+	  }
+}
